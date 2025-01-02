8b0b1fde6334eb6f6fced6d4238992272b7a8455
Pubudu11 <pubuduganganodya@gmail.com>
2025-01-03 02:35:21 +0530

    making admin model

---
+++ src/main/java/com/example/bookstore/Model/Admin.java
@@ -0,0 +1,8 @@
+package com.example.bookstore.Model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import
+
+public class Admin {
+}
