c211dc628139585cc9fba6c29e2aa2edd45cc888
Pubudu11 <pubuduganganodya@gmail.com>
2025-01-03 03:45:51 +0530

    working on admin module

--- src/main/java/com/example/bookstore/Model/Admin.java
+++ src/main/java/com/example/bookstore/Model/Admin.java
@@ -1,8 +1,16 @@
 package com.example.bookstore.Model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import oswa
+import org.springframework.web.bind.annotation.*;
+
+@Document(collection = "admins")
+@Data
 
 public class Admin {
+    @Id
+    private String id;
+    private String username;
+    private String password;
 }
