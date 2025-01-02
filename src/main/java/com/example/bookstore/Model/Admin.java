95f860ec6565a5f86926e4b233975a64f0aa1ca4
Pubudu11 <pubuduganganodya@gmail.com>
2025-01-03 03:25:26 +0530

    update

--- src/main/java/com/example/bookstore/Model/Admin.java
+++ src/main/java/com/example/bookstore/Model/Admin.java
@@ -2,7 +2,7 @@ package com.example.bookstore.Model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import os
+import oswa
 
 public class Admin {
 }
