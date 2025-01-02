4de4d28a6e5b74c934c8749e0d04d0257f053615
Pubudu11 <pubuduganganodya@gmail.com>
2025-01-03 02:38:13 +0530

    🚧

--- src/main/java/com/example/bookstore/Model/Admin.java
+++ src/main/java/com/example/bookstore/Model/Admin.java
@@ -2,7 +2,7 @@ package com.example.bookstore.Model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import
+import o
 
 public class Admin {
 }
