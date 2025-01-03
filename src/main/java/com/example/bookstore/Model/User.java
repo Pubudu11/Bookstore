426efc267eb3f1f0be0cc5c88600454ad74348a0
Pubudu11 <pubuduganganodya@gmail.com>
2025-01-03 06:43:47 +0530

    🦺 🔒️ ✅ implementing jwt authentication

--- src/main/java/com/example/bookstore/Model/User.java
+++ src/main/java/com/example/bookstore/Model/User.java
@@ -3,6 +3,14 @@ package com.example.bookstore.Model;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Document(collection = "users")
 @Data
@@ -10,12 +18,26 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private String id;
+
     private String firstName;
     private String lastName;
+    @NotBlank
+    @Size(min = 5, max = 50)
     private String username;
+    @NotBlank
+    @Size(min = 5, max = 50)
+
     private String password;
+
+    @Size(min = 10, max = 100)
+    @Email
     private String email;
+
+
     private String phone;
+
+    @DBRef
+    private Set<Role> roles = new HashSet<>();
 
     public String getId() {
         return id;
@@ -71,5 +93,13 @@ public class User {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
