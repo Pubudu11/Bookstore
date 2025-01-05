2e643544241a94fef6c6950e345490b86be0bd94
Sithmi43 <apsarak-ps20043@stu.kln.ac.lk>
2025-01-06 01:41:20 +0530

    fix

--- bookstore-frontend/src/main.tsx
+++ bookstore-frontend/src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!)
+  .render(
 
-  </React.StrictMode>
-)
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+
+    </React.StrictMode>
+  )
