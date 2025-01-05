834c0728e4ba0edd0fea29b67e6cb40cfbee8ee7
Sithmi43 <apsarak-ps20043@stu.kln.ac.lk>
2025-01-06 01:11:35 +0530

    create home

--- bookstore-frontend/src/App.tsx
+++ bookstore-frontend/src/App.tsx
@@ -1,12 +1,14 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
 
 function App() {
+  return <Container>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
 
-  return (
-    <>
-      <h1>Welocome</h1>
-      <h1>Book</h1>
-    </>
-  )
+    </Routes>
+  </Container>
 }
-
 export default App
