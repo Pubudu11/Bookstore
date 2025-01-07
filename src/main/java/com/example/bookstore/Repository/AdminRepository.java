85f9099b7e5c10775f5dea6c0cbd68e1fff9dfc2
Pubudu11 <pubuduganganodya@gmail.com>
2025-01-07 10:29:15 +0530

    ♻️ Add repository annotations and methods for Admin and Role repositories

--- src/main/java/com/example/bookstore/Repository/AdminRepository.java
+++ src/main/java/com/example/bookstore/Repository/AdminRepository.java
@@ -3,8 +3,14 @@ package com.example.bookstore.Repository;
 import com.example.bookstore.Model.Admin;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
+@Repository
 public interface AdminRepository extends MongoRepository<Admin, String> {
+    Optional<Admin> findByUsername(String username);
 
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
